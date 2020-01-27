6c75688f0c7db564d8bb57c782618e1d12a66828
yutachaos <y_kimura@r.recruit.co.jp>
2020-01-27 15:09:04 +0900

    chore: implements triggers unit tests (#16)

--- triggers/triggers_test.go
+++ triggers/triggers_test.go
@@ -49,3 +49,71 @@ func TestGetTriggers(t *testing.T) {
 	assert.Equal(t, "the title: test", notification.Title)
 	assert.Equal(t, "the body: test", notification.Body)
 }
+
+func TestGetTriggers_UsingContext(t *testing.T) {
+	triggers, err := GetTriggers([]NotificationTemplate{{
+		Name: "template",
+		Notification: notifiers.Notification{
+			Title: "the title: {{.app.metadata.name}}",
+			Body:  "the body: {{.app.metadata.name}} argocd url: {{.context.argocdUrl}}",
+		},
+	}}, []NotificationTrigger{{
+		Name:      "trigger",
+		Template:  "template",
+		Condition: "app.metadata.name == 'foo'",
+	}})
+	assert.NoError(t, err)
+
+	trigger, ok := triggers["trigger"]
+	assert.True(t, ok)
+
+	ok, err = trigger.Triggered(testingutil.NewApp("foo"))
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	ok, err = trigger.Triggered(testingutil.NewApp("bar"))
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	notification, err := trigger.FormatNotification(testingutil.NewApp("test"), map[string]string{"argocdUrl": "testUrl"})
+	assert.NoError(t, err)
+	assert.Equal(t, "the title: test", notification.Title)
+	assert.Equal(t, "the body: test argocd url: testUrl", notification.Body)
+}
+
+func TestGetTriggers_UsingSlack(t *testing.T) {
+	triggers, err := GetTriggers([]NotificationTemplate{{
+		Name: "template",
+		Notification: notifiers.Notification{
+			Title: "the title: {{.app.metadata.name}}",
+			Body:  "the body: {{.app.metadata.name}}",
+			Slack: &notifiers.SlackSpecific{
+				Attachments: "Application {{.app.metadata.name}} Application details: {{.context.argocdUrl}}/applications/{{.app.metadata.name}}.",
+				Blocks:      "Application {{.app.metadata.name}} Application details: {{.context.argocdUrl}}/applications/{{.app.metadata.name}}.",
+			},
+		},
+	}}, []NotificationTrigger{{
+		Name:      "trigger",
+		Template:  "template",
+		Condition: "app.metadata.name == 'foo'",
+	}})
+	assert.NoError(t, err)
+
+	trigger, ok := triggers["trigger"]
+	assert.True(t, ok)
+
+	ok, err = trigger.Triggered(testingutil.NewApp("foo"))
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	ok, err = trigger.Triggered(testingutil.NewApp("bar"))
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	notification, err := trigger.FormatNotification(testingutil.NewApp("test"), map[string]string{"argocdUrl": "testUrl"})
+	assert.NoError(t, err)
+	assert.Equal(t, "the title: test", notification.Title)
+	assert.Equal(t, "the body: test", notification.Body)
+	assert.Equal(t, "Application test Application details: testUrl/applications/test.", notification.Slack.Attachments)
+	assert.Equal(t, "Application test Application details: testUrl/applications/test.", notification.Slack.Blocks)
+}
