0ba3ad1eec854fb9011ae50544e4e8d53c782006
Nicolas Huray <nicolas.huray@gmail.com>
2020-03-10 02:01:33 -0400

    Grafana annotation notifier (#53)
    * Create a notifier for Grafana in order to create Grafana annotation
    
    * Update settings_test.go with Grafana config
    
    * Update documentation
    
    * Update notifiers/grafana.go
    
    Co-Authored-By: Bot from GolangCI <42910462+golangcibot@users.noreply.github.com>
    
    * add missing log import
    
    Co-authored-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Co-authored-by: Bot from GolangCI <42910462+golangcibot@users.noreply.github.com>

---
+++ docs/services/grafana.md
@@ -0,0 +1,22 @@
+# Grafana
+
+To be able to create Grafana annotation with argocd-notifications you have to create an [API Key](https://grafana.com/docs/grafana/latest/http_api/auth/#create-api-key) inside your [Grafana](https://grafana.com).
+
+1. Login to your Grafana instance as `admin`
+2. On the left menu, go to Configuration / API Keys
+3. Click "Add API Key" 
+4. Fill the Key with name `ArgoCD Notification`, role `Editor` and Time to Live `10y` (for example)
+5. Click on Add button
+6. Copy your API Key and define it in `notifiers.yaml`
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: argocd-notifications-secret
+stringData:
+  notifiers.yaml: |
+    grafana:
+      apiUrl: https://grafana.example.com/api
+      apiKey: <grafana-api-key> 
+```
---
+++ notifiers/grafana.go
@@ -0,0 +1,73 @@
+package notifiers
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type GrafanaOptions struct {
+	ApiUrl             string `json:"apiUrl"`
+	ApiKey             string `json:"apiKey"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify"`
+}
+
+type grafanaNotifier struct {
+	opts GrafanaOptions
+}
+
+func NewGrafanaNotifier(opts GrafanaOptions) Notifier {
+	return &grafanaNotifier{opts: opts}
+}
+
+type GrafanaAnnotation struct {
+	Time     int64    `json:"time"` // unix ts in ms
+	IsRegion bool     `json:"isRegion"`
+	Tags     []string `json:"tags"`
+	Text     string   `json:"text"`
+}
+
+func (n *grafanaNotifier) Send(notification Notification, tags string) error {
+	ga := GrafanaAnnotation{
+		Time:     time.Now().Unix() * 1000, // unix ts in ms
+		IsRegion: false,
+		Tags:     strings.Split(tags, "|"),
+		Text:     notification.Title,
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: n.opts.InsecureSkipVerify,
+			},
+		},
+	}
+
+	jsonValue, _ := json.Marshal(ga)
+	apiUrl, err := url.Parse(n.opts.ApiUrl)
+
+	if err != nil {
+		return err
+	}
+	annotationApi := *apiUrl
+	annotationApi.Path = path.Join(apiUrl.Path, "annotations")
+	req, err := http.NewRequest("POST", annotationApi.String(), bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Errorf("Failed to create grafana annotation request: %s", err)
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", n.opts.ApiKey))
+
+	_, err = client.Do(req)
+	return err
+}
