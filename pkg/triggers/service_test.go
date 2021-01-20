24ca9c30572d07137690c29597a5e733739ff55d
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-01-20 12:09:14 -0800

    fix: the on-deployed trigger sends multiple notifications (#154)

--- pkg/triggers/service_test.go
+++ pkg/triggers/service_test.go
@@ -57,3 +57,46 @@ func TestRun(t *testing.T) {
 		}}, res)
 	})
 }
+
+func TestRun_OncePerSet(t *testing.T) {
+	revision := "123"
+	svc, err := NewService(map[string][]Condition{
+		"my-trigger": {{
+			When:    "var1 == 'abc'",
+			Send:    []string{"my-template"},
+			OncePer: "revision",
+		}},
+	})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	conditionKey := fmt.Sprintf("%s:[0].%s", revision, hash("var1 == 'abc'"))
+
+	t.Run("Triggered", func(t *testing.T) {
+		res, err := svc.Run("my-trigger", map[string]interface{}{"var1": "abc", "revision": "123"})
+		if assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, []ConditionResult{{
+			Key:       conditionKey,
+			Triggered: true,
+			Templates: []string{"my-template"},
+			OncePer:   revision,
+		}}, res)
+	})
+
+	t.Run("NotTriggered", func(t *testing.T) {
+		res, err := svc.Run("my-trigger", map[string]interface{}{"var1": "bcd"})
+		if assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, []ConditionResult{{
+			Key:       conditionKey,
+			Triggered: false,
+			Templates: []string{"my-template"},
+			OncePer:   revision,
+		}}, res)
+	})
+}
