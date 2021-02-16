fd40e5f71327c40a975cf7d1aeec13118483bdef
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2021-02-17 02:35:53 +0900

    add replaceServiceConfigSecret test

--- pkg/config_test.go
+++ pkg/config_test.go
@@ -56,3 +56,61 @@ func TestReplaceStringSecret_KeyMissing(t *testing.T) {
 
 	assert.Equal(t, "hello $secret-value", val)
 }
+
+func TestReplaceServiceConfigSecret(t *testing.T) {
+	tests := []struct {
+		config map[string]interface{}
+		secret map[string][]byte
+		want   map[string]interface{}
+	}{
+		{
+			config: map[string]interface{}{
+				"url": "$endpoint",
+				"headers": []map[string]interface{}{
+					{
+						"name":  "Authorization",
+						"value": "Bearer $secret-value",
+					},
+				},
+			},
+			secret: map[string][]byte{
+				"endpoint":     []byte("https://example.com"),
+				"secret-value": []byte("token"),
+			},
+			want: map[string]interface{}{
+				"url": "https://example.com",
+				"headers": []map[string]interface{}{
+					{
+						"name":  "Authorization",
+						"value": "Bearer token",
+					},
+				},
+			},
+		},
+		{
+			config: map[string]interface{}{
+				"apiUrl": "$endpoint",
+				"apiKeys": map[string]interface{}{
+					"first-team":  "$first-team-secret",
+					"second-team": "$second-team-secret",
+				},
+			},
+			secret: map[string][]byte{
+				"first-team-secret":  []byte("first-token"),
+				"second-team-secret": []byte("second-token"),
+			},
+			want: map[string]interface{}{
+				"apiUrl": "$endpoint",
+				"apiKeys": map[string]interface{}{
+					"first-team":  "first-token",
+					"second-team": "second-token",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		result := replaceServiceConfigSecret(tt.config, tt.secret)
+		assert.Equal(t, tt.want, result)
+	}
+}
