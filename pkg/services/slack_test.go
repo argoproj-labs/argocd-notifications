2debf0d5e9a9d34989c31d625635d6d6d86f5378
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2021-03-26 23:43:41 +0900

    feat: support change timezone
    Signed-off-by: Ryota Sakamoto <sakamo.ryota+github@gmail.com>

--- pkg/services/slack_test.go
+++ pkg/services/slack_test.go
@@ -1,10 +1,14 @@
 package services
 
 import (
+	"os"
 	"testing"
 	"text/template"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+
+	et "github.com/argoproj-labs/argocd-notifications/expr/time"
 )
 
 func TestValidIconEmoij(t *testing.T) {
@@ -46,4 +50,44 @@ func TestGetTemplater_Slack(t *testing.T) {
 
 	assert.Equal(t, "hello", notification.Slack.Attachments)
 	assert.Equal(t, "world", notification.Slack.Blocks)
+}
+
+func TestChangeTimezone_Slack(t *testing.T) {
+	tz := os.Getenv("TZ")
+	if err := os.Setenv("TZ", "Asia/Tokyo"); err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := os.Setenv("TZ", tz); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	n := Notification{
+		Slack: &SlackNotification{
+			Attachments: `{{ (call .time.Parse .date).Local.Format "2006-01-02T15:04:05Z07:00" }}`,
+		},
+	}
+	templater, err := n.GetTemplater("", template.FuncMap{})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	currentTime, err := time.Parse(time.RFC3339, "2021-03-26T23:38:01Z")
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	var notification Notification
+	err = templater(&notification, map[string]interface{}{
+		"date": currentTime.Format(time.RFC3339),
+		"time": et.NewExprs(),
+	})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, "2021-03-27T08:38:01+09:00", notification.Slack.Attachments)
 }
